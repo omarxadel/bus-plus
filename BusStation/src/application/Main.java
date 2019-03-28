e5d61c7b28911424536dbd9ccae0864196044692
omarxadel <48330931+omarxadel@users.noreply.github.com>
2019-03-28 20:38:08 +0200

    Added GUI (Sample, HomeScreen,..)

--- BusStation/src/application/Main.java
+++ BusStation/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,8 +12,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			primaryStage.setTitle("B U S");
+			TabPane root = (TabPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,800,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- BusStation/src/application/SampleController.java
+++ BusStation/src/application/SampleController.java
@@ -1,5 +1,9 @@
 package application;
+import javafx.scene.*;
+
 
 public class SampleController {
-	
+	public void loginButtonClicked() {
+		
+	}
 }
