b96d90daf92ad23fecc51b7bdd20c65c5466abea
omarxadel <48330931+omarxadel@users.noreply.github.com>
2019-04-08 06:32:40 +0200

    Manager got some errors commiting for a pull

--- BusStation/src/application/Manager.java
+++ BusStation/src/application/Manager.java
@@ -15,8 +15,8 @@ public class Manager extends Users {
 	
 
 	public Trip addTrip(String s, String d, String v, String dr, String da, Seat S) {
-		Trip trip = new Trip(s, d, v, dr, da, S);
-
+		Trip trip = new Trip(s, d, v,   dr, da, S);
+	}
 	public Trip createTrip(String s, String d, String v, int vnum, String dr, String da, float ticket) {
 		Seat S = new Seat(v);
 		Trip trip = new Trip(s, d, v, vnum, dr, da, ticket, S);
