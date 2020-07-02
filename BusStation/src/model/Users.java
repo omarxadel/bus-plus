d33a5c6d27c9de34a4e138998da2b0e39c5bb949
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2020-07-02 02:29:41 +0200

    Update attributes access visibility to private in Users.java

--- BusStation/src/model/Users.java
+++ BusStation/src/model/Users.java
@@ -1,21 +1,88 @@
 package model;
 
 public abstract class Users {
-	String username;
+	private String username;
 	private String password;
-	String firstname;
-	String lastname;
-	String city;
-	String country;
-	String job;
-	String gender;
-	int ID;
-	
+	private String firstname;
+	private String lastname;
+	private String city;
+	private String country;
+	private String job;
+	private String gender;
+	private int ID;
+
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
 	String getPassword() {
+
 		return password;
 	}
 	
 	void setPassword(String password) {
+
 		this.password=password;
 	}
 }
