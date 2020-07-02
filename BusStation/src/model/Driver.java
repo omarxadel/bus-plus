09ee25c1261eeaa1748e1d8bbe204e5ee1ce30d9
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2020-07-02 02:53:02 +0200

    redundant commit

--- BusStation/src/model/Driver.java
+++ BusStation/src/model/Driver.java
@@ -2,9 +2,9 @@ package model;
 import java.util.*;
 
 public class Driver extends Users {
-	HashMap<Integer, Trip> trip = new HashMap<>();
+	private HashMap<Integer, Trip> trip = new HashMap<>();
 	private float credit;
-	String salary;
+	//private float salary;
 	
 	public Driver(String fname, String lname, String uname, String pw, int ID, String city, String country, String job) {
 		Driver.super.firstname = fname;
@@ -24,7 +24,16 @@ public class Driver extends Users {
 	}
 
 	public void setCredit(float credit) {
+
 		this.credit = credit;
+	}
+
+	public HashMap<Integer , Trip> getTrip {
+		return trip;
+	}
+
+	public void setTrip(HashMap<Intege , Trip>){
+		this.trip=trip;
 	}
 	
 	
