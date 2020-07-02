cc383248d9ed82edb7d995b0a56ff1ef9078c069
Hoda Amr <48572265+hodaamr@users.noreply.github.com>
2020-07-02 03:58:18 +0200

    Update PaymentMethod (private variables)

--- BusStation/src/Interface/PaymentMethod.java
+++ BusStation/src/Interface/PaymentMethod.java
@@ -1,5 +1,6 @@
 package Interface;
 
 public interface PaymentMethod {
+
 	public void pay(float amount, String code);
 }
--- BusStation/src/model/Cash.java
+++ BusStation/src/model/Cash.java
@@ -3,11 +3,20 @@ package model;
 import Interface.PaymentMethod;
 
 public class Cash implements PaymentMethod {
-	float toPay;
+	 private float toPay;
 	
 	@Override
 	public void pay(float amount, String code) {
+
 		toPay = amount;
+	}
+
+	public float getToPay() {
+		return toPay;
+	}
+
+	public void setToPay(float toPay) {
+		this.toPay = toPay;
 	}
 }
 
