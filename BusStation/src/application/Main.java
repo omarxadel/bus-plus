af9167c9d61ab3db1b827d0c3b26ca99e2dd0504
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2020-07-08 12:03:30 +0200

    Update Main.java
    Add draggable property to the window

--- BusStation/src/application/Main.java
+++ BusStation/src/application/Main.java
@@ -8,8 +8,13 @@ import javafx.fxml.FXMLLoader;
 import animatefx.animation.*;
 import javafx.stage.StageStyle;
 
+import java.awt.event.MouseEvent;
+
 
 public class Main extends Application {
+	double xOffset;
+	double yOffset;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -17,9 +22,26 @@ public class Main extends Application {
 			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("../view/MainMenu.fxml"));
 			Scene scene = new Scene(root,720,435);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+
+
+
 			primaryStage.setScene(scene);
+
+			// MOVE THE WINDOW
+			root.setOnMousePressed(mouseEvent -> {
+				xOffset = primaryStage.getX() - mouseEvent.getScreenX();
+				yOffset = primaryStage.getY() - mouseEvent.getScreenY();
+			});
+
+			root.setOnMouseDragged(mouseEvent -> {
+				primaryStage.setX(mouseEvent.getScreenX() + xOffset);
+				primaryStage.setY(mouseEvent.getScreenY() + yOffset);
+			});
+
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
-			new animatefx.animation.FadeIn(root).play();
+			new FadeIn(root).play();
+
 			primaryStage.show();
 			
 		} catch(Exception e) {
