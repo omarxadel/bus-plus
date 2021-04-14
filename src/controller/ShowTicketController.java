12f4b273236d15cb519dc2679c93d66d4b6d5044
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2021-04-14 16:10:45 +0200

    Update project structure

---
+++ src/controller/ShowTicketController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.scene.control.Label;
+
+public class ShowTicketController{
+	Label passengername, S,D,Destination, Time,Date, Serial;
+	
+	
+	public void addData(String passenger, String Start, String Destination,String Time,String Date,String Serial) {
+		
+	}
+}
